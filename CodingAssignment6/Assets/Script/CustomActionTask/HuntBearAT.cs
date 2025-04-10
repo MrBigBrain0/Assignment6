69ba2803b4c9ea46c813b718023d35a5f9595024
MrBigBrain0 <148355534+MrBigBrain0@users.noreply.github.com>
2025-04-10 13:50:33 -0400

    Hunter Update
    - hunter works and is programed yay!

--- CodingAssignment6/Assets/Script/CustomActionTask/HuntBearAT.cs
+++ CodingAssignment6/Assets/Script/CustomActionTask/HuntBearAT.cs
@@ -1,36 +1,41 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using UnityEngine;
 
 
-namespace NodeCanvas.Tasks.Actions {
+namespace NodeCanvas.Tasks.Actions 
+{
 
-	public class HuntBearAT : ActionTask {
-
-		//Use for initialization. This is called only once in the lifetime of the task.
-		//Return null if init was successfull. Return an error string otherwise
-		protected override string OnInit() {
+	public class HuntBearAT : ActionTask
+	{
+		public BBParameter <GameObject> p;
+		public BBParameter <GameObject> h;
+		protected override string OnInit() 
+		{
 			return null;
 		}
 
-		//This is called once each time the task is enabled.
-		//Call EndAction() to mark the action as finished, either in success or failure.
-		//EndAction can be called from anywhere.
-		protected override void OnExecute() {
-			EndAction(true);
-		}
-
-		//Called once per frame while the action is active.
-		protected override void OnUpdate() {
+		protected override void OnExecute() 
+		{
 			
 		}
 
-		//Called when the task is disabled.
-		protected override void OnStop() {
+		protected override void OnUpdate() 
+		{
+			p.value.transform.position = new Vector3(-0.3002234f, 1.22f, -23.92f);
+			h.value.transform.position = new Vector3(20.14f, 0.44f, 4.61f);
+
+        }
+
+
+		protected override void OnStop() 
+		{
 			
 		}
 
-		//Called when the task is paused.
-		protected override void OnPause() {
+
+		protected override void OnPause() 
+		{
 			
 		}
 	}
